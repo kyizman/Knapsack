e4415e72b0f7cf7f877f573e1e895a0cefe5f605
kyson <kyson@DESKTOP-TODPT99>
2021-11-24 16:47:44 -0500

    s

--- Knapsack/Genetic.cs
+++ Knapsack/Genetic.cs
@@ -15,7 +15,11 @@ namespace Knapsack
         private int numberofitems = 0;
         private int maxgenerations = 0;
         private int generationcount = 1;
-
+        private double knapsack_capacity = 0;
+        private double prob_crossover = 0;
+        private double prob_mutation = 0;
+        private double totalfitnessofgen = 0;
+        private array
 
     }
 }
